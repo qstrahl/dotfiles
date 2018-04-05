ccc91159a97f5a60661a724bb347e8cf6159837d
Quinn Strahl <qstrahl@gmail.com>
2018-04-05 17:32:12 -0400

    Update tmuxifier sessions

--- tmuxifier-layouts/.tmuxifier/layouts/update.session.sh
+++ tmuxifier-layouts/.tmuxifier/layouts/update.session.sh
@@ -1,15 +1,12 @@
 if initialize_session "update"; then
   new_window
-  run_cmd "pacaur -Syu"
+  run_cmd "aurman -Syu"
 
   new_window
   run_cmd "antigen update"
 
   new_window
-  run_cmd "vim +PlugUpdate"
-
-  new_window
-  run_cmd "tmux run-shell $HOME/.tmux/plugins/tpm/bindings/update_plugins"
+  run_cmd "nvim +PlugUpdate"
 fi
 
 # Finalize session creation and switch/attach to it.
