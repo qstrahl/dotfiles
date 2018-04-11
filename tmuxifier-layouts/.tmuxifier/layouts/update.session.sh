cedbd9026eda4867d1c018a3cf53b0c5bf377427
Quinn Strahl <qstrahl@gmail.com>
2018-04-11 10:43:17 -0400

    Add tpm update to tmuxifier update session

--- tmuxifier-layouts/.tmuxifier/layouts/update.session.sh
+++ tmuxifier-layouts/.tmuxifier/layouts/update.session.sh
@@ -7,6 +7,10 @@ if initialize_session "update"; then
 
   new_window
   run_cmd "nvim +PlugUpdate"
+
+  new_window
+  run_cmd "tmux run-shell '$HOME/.tmux/plugins/tpm/scripts/update_plugin_prompt_handler.sh all'"
+  
 fi
 
 # Finalize session creation and switch/attach to it.
