6113438d9d644ac729f71538cd5d09a0e1cd5a1d
Quinn Strahl <qstrahl@gmail.com>
2018-04-12 11:15:38 -0400

    Automate nvim :PlugDiff after :PlugUpdate in tmux update session

--- tmuxifier-layouts/.tmuxifier/layouts/update.session.sh
+++ tmuxifier-layouts/.tmuxifier/layouts/update.session.sh
@@ -6,7 +6,7 @@ if initialize_session "update"; then
   run_cmd "antigen update"
 
   new_window
-  run_cmd "nvim +PlugUpdate"
+  run_cmd "nvim +PlugUpdate +PlugDiff"
 
   new_window
   run_cmd "tmux run-shell '$HOME/.tmux/plugins/tpm/scripts/update_plugin_prompt_handler.sh all'"
