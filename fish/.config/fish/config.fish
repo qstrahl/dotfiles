978cd9107d43970498beb15111e36c99cef87d63
Quinn Strahl <qstrahl@gmail.com>
2021-02-22 11:05:40 -0500

    Comment out startx bootstrap in config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-status is-login && test -z $DISPLAY && test (tty) = "/dev/tty1" && exec startx -- -keeptty
+# status is-login && test -z $DISPLAY && test (tty) = "/dev/tty1" && exec startx -- -keeptty
 
 functions -q fzf_key_bindings && fzf_key_bindings
 bind -M default -M insert \cj history-search-forward
