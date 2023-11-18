017cec20184b7e0f4a9a85f924e7b9a6ff9aa90c
Quinn Strahl <qstrahl@gmail.com>
2023-11-17 20:39:02 -0500

    Add extended keys to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,15 @@
 # status is-login && test -z $DISPLAY && test (tty) = "/dev/tty1" && exec startx -- -keeptty
 
 functions -q fzf_key_bindings && fzf_key_bindings
+
+# extended keys
+set -l cTab \e\[9\;5u
+set -l cEnter \e\[13\;5u
+set -l sEnter \e\[13\;2u
+
 bind -M default -M insert \cj history-search-forward
 bind -M default -M insert \ck history-search-backward
+bind -M insert $cTab accept-autosuggestion
+bind -M insert $cEnter accept-autosuggestion and execute
 
 starship init fish | source
