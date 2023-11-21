902dffb770e4a6d65efa923e593be8e599c3e5ae
Quinn Strahl <qstrahl@gmail.com>
2023-11-21 14:17:50 -0500

    Set fish cursor shapes at same place theme is chosen

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,12 +5,6 @@ if status is-interactive
   # fzf keys, maybe
   functions -q fzf_key_bindings && fzf_key_bindings
 
-  # cursor shape
-  set fish_cursor_default block blink
-  set fish_cursor_insert line blink
-  set fish_cursor_replace_one underscore blink
-  set fish_cursor_visual block blink
-
   # extended keys
   set -l cTab \e\[9\;5u
   set -l cEnter \e\[13\;5u
@@ -20,6 +14,12 @@ if status is-interactive
   bind -M default -M insert \ck history-search-backward
   bind -M insert $cTab accept-autosuggestion
   bind -M insert $cEnter accept-autosuggestion and execute
+
+  # cursor shape
+  set fish_cursor_default block blink
+  set fish_cursor_insert line blink
+  set fish_cursor_replace_one underscore blink
+  set fish_cursor_visual block blink
 
   fish_config theme choose mine
 
