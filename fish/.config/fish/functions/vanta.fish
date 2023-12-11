6555da2e47570a9e60e5718fb1c1ebd1b07d5a1f
Quinn Strahl <qstrahl@gmail.com>
2023-12-11 11:58:48 -0500

    Use absolute path for cd into vanta workspace

--- fish/.config/fish/functions/vanta.fish
+++ fish/.config/fish/functions/vanta.fish
@@ -1,5 +1,5 @@
 function vanta -d "Start up dev stuff in vanta codespace"
-  cd obsidian && mux vanta \
+  cd ~/obsidian && mux vanta \
     neww lazydocker lazydocker \
     neww make 'make dev-start-web' \
     neww nvim nvim
