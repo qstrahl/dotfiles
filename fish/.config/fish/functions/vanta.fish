bac8993f62f46d1ea8e2ed2e2ebe9e89f52ee265
Quinn Strahl <qstrahl@gmail.com>
2024-04-02 10:29:19 -0400

    No I mean really fix it this time

--- fish/.config/fish/functions/vanta.fish
+++ fish/.config/fish/functions/vanta.fish
@@ -1,6 +1,6 @@
 function vanta -d "Start up dev stuff in vanta codespace"
   cd ~/obsidian && mux vanta \
     neww lazydocker lazydocker \
-    neww storybook 'env BROWSER=echo make storybook' \
+    neww storybook 'env BROWSER=echo make dev-storybook' \
     neww nvim nvim
 end
