5abd3664e5e39352fc0efbb675c0090a84f868fb
Quinn Strahl <qstrahl@gmail.com>
2024-04-02 10:28:23 -0400

    Fix storybook window in vanta tmux function

--- fish/.config/fish/functions/vanta.fish
+++ fish/.config/fish/functions/vanta.fish
@@ -1,6 +1,6 @@
 function vanta -d "Start up dev stuff in vanta codespace"
   cd ~/obsidian && mux vanta \
     neww lazydocker lazydocker \
-    neww storybook storybook \
+    neww storybook 'env BROWSER=echo make storybook' \
     neww nvim nvim
 end
