8a1d6df959d9207d179d974ad99c62cc3caf7a1a
Quinn Strahl <qstrahl@gmail.com>
2025-11-18 13:56:27 -0500

    Don't run storybook by default

--- fish/.config/fish/functions/vanta.fish
+++ fish/.config/fish/functions/vanta.fish
@@ -1,6 +1,5 @@
 function vanta -d "Start up dev stuff in vanta codespace"
   cd ~/obsidian && mux vanta \
     neww lazydocker lazydocker \
-    neww storybook 'env BROWSER=echo just dev-storybook' \
     neww nvim nvim
 end
